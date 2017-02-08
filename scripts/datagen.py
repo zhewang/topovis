a53f7793e8096d961a5c16a0f12ea067f194df51
Zhe Wang <zwang.contact@gmail.com>
2017-02-07 23:03:39 -0700

    initial commit

---
+++ scripts/datagen.py
@@ -0,0 +1,45 @@
+import csv
+import math
+import random
+
+#base vector
+px = 1.
+py = 0.
+qx = 0.5
+qy = math.sqrt(3)/2.
+
+l = 40  # the length of the qudrilateral
+r = l*math.sqrt(3)/4.  # the circle inside the mesh quadrilateral
+# center of the circle
+cx = l*3./4.
+cy = r
+
+points = []
+for i in range(l):
+    for j in range(l):
+        tx = px*i+qx*j
+        ty = py*i+qy*j
+
+        if (tx-cx)**2 + (ty-cy)**2 < r**2:
+            points.append([tx, ty, 1])
+
+# randomly choose 100 points to be category 2
+for i in range(int(0.2*len(points))):
+    index = random.randint(0, len(points)-1)
+    points[index][2] = 2
+
+# choose an inner circle to be category 3
+# choose an intercetion circle to be category 4
+for point in points:
+    if (point[0]-0.8*cx)**2 + (point[1]-cy)**2 < (r/3.)**2:
+        point[2] = 3
+
+    if (point[0]-1.1*l)**2 + (point[1]-cy)**2 < (r/3.)**2:
+        point[2] = 4
+
+# write to csv file
+with open('mesh.csv', "wb") as file:
+    writer = csv.writer(file)
+    writer.writerow(['px','py','c'])
+    for point in points:
+        writer.writerow(point)
---
+++ webui/scatterplot.js
@@ -0,0 +1,88 @@
+d3.csv('mesh.csv', function(data) {
+
+    // load data
+    for (var i = 0; i < data.length; i ++) {
+        data[i].px = Number(data[i].px);
+        data[i].py = Number(data[i].py);
+        data[i].c = Number(data[i].c);
+    }
+
+    // plot
+    var margin = {top: 20, right: 20, bottom: 20, left: 20}
+        , width = 600 - margin.left - margin.right
+        , height = 600 - margin.top - margin.bottom;
+
+    var xMax = d3.max(data, function(d) { return d.px; });
+    var yMax = d3.max(data, function(d) { return d.py; });
+    var totalMax = d3.max([xMax, yMax])+1;
+
+    var x = d3.scaleLinear()
+        .domain([0, totalMax])
+        .range([ 0, width ]);
+
+    var y = d3.scaleLinear()
+        .domain([0, totalMax])
+        .range([ height, 0 ]);
+
+    var colorscale = d3.scaleOrdinal()
+        .domain([1,2,3,4])
+        .range(['#377eb8','#e41a1c','#4daf4a','#984ea3']);
+
+    var chart = d3.select('#scatterplot')
+        .append('svg:svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+        .attr('class', 'chart')
+
+    var main = chart.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('class', 'main')
+
+    // draw the x axis
+    var xAxis = d3.axisBottom(x);
+
+    main.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .attr('class', 'main axis date')
+        .call(xAxis);
+
+    // draw the y axis
+    var yAxis = d3.axisLeft(y);
+
+    main.append('g')
+        .attr('transform', 'translate(0,0)')
+        .attr('class', 'main axis date')
+        .call(yAxis);
+
+    var g = main.append("svg:g");
+
+    // create a default filter
+    var catSelection = [true, true, true, true];
+
+    var plotDots = function (sel) {
+        sel.append("circle")
+        .attr("cx", function (d) { return x(d.px); } )
+        .attr("cy", function (d) { return y(d.py); } )
+        .attr("fill", function (d) { return colorscale(d.c); })
+        .attr("r", 4);
+    };
+
+    g.selectAll("circle")
+        .data(data)
+        .enter()
+        .call(plotDots);
+
+    // selection part
+    d3.selectAll("input").on('change', function () {
+        catSelection[this.value-1] = this.checked ? true:false;
+
+        d3.selectAll("circle").remove();
+        d3.select('#scatterplot').select('svg').selectAll("circle")
+            .data(data)
+            .enter()
+            .filter(function(d) { return catSelection[d.c-1]; })
+            .call(plotDots);
+    });
+});
