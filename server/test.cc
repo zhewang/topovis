e95ff9c4cf1557b41a8840cc9d556d131b28ba5f
Zhe Wang <zwang.contact@gmail.com>
2017-03-17 14:08:00 -0700

    add simple test

---
+++ server/test.cc
@@ -0,0 +1,9 @@
+#include "topology/persistence.h"
+#include "topology/sparse_rips_filtration.h"
+#include "topology/rips_filtration.h"
+#include "topology/fixed_filtration.h"
+
+int main() {
+    std::cout << "test" << std::endl;
+    return 0;
+}
